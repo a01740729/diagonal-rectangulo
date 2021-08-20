f4684447d99cb34d70cad7cb70d23a59d50a082c
a01740729 <a01740729@itesm.mx>
2021-08-20 01:38:27 +0000

    diagonal rectangulo

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,6 +1,14 @@
+import math
 def main():
     #escribe tu código abajo de esta línea
     pass
+
+    a = float(input('Dame el ancho del rectangulo: '))
+    l = float(input('Dame el largo del rectangulo: '))
+
+    d = math.sqrt ((a ** 2) + (l ** 2))
+
+    print('La diagonal es: '+str(d))
 
 if __name__=='__main__':
     main()
